018fd640055d582003db772109eddc79f1025d61
provided <provided@provideds-Mac-mini.local>
2025-11-19 11:36:42 +0700

    cho loc ngu cho

--- internal/shared/interface.go
+++ internal/shared/interface.go
@@ -5,8 +5,6 @@ import (
 	"time"
 )
 
-// AttendanceRepository defines methods needed to fetch student attendance
-// data for shared use-cases (e.g., temperature chart).
 type AttendanceRepository interface {
 	GetStudentAttendanceInDateRange(c context.Context, studentID string, startDate time.Time, endDate time.Time) ([]*AttendanceStudent, error)
 }
